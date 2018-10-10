16832417afc3fa489e1fcadebda6129abc042b81
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-10-11 01:41:37 +0200

    removed file date usage completely, implemented basic filedate type

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,21 @@ import {drive} from "./drive";
 import {drives} from "./driveConfig";
 import {firebaseConfig} from "./firebaseConfig";
 
-httpHandler.get({url: firebaseConfig.url}).then(result => {
-    const collection = new ImageCollection();
-    console.log(Object.keys(JSON.parse(result).data).length);
-//    collection.setInitialData(JSON.parse(result));
-    console.log("stuff added");
-    Promise.all(drives.map(config => new drive(config.key, config.folder).driveResult))
-        .then(results =>
-            results.map(driveResult => collection.addImages(driveResult.files, driveResult.apiKey)))
-        .then(() => console.log("data grabbed and processed ;)"))
-        .then(async () => await httpHandler.put({
-            url: `${firebaseConfig.url}?print=silent`,
-            body: JSON.stringify(collection)
-        })).then(x => console.log("success:" + x+ " dt"+Object.keys(JSON.parse(JSON.stringify(collection)).data).length+" notadded:"+collection.notAddedCounter)).catch(err => console.error(err));
-}).catch(err => console.error(err));
+console.log('drive indexer started');
+httpHandler.get({url: firebaseConfig.url})
+    .then(result => {
+        const collection = new ImageCollection();
+        console.log(Object.keys(JSON.parse(result).data).length);
+        console.log("stuff added");
+        Promise.all(drives.map(config => new drive(config.key, config.folder).driveResult))
+            .then(results =>
+                results.map(driveResult => collection.addImages(driveResult.files, driveResult.apiKey)))
+            .then(() => console.log("data grabbed and processed ;)"))
+            .then(async () =>
+                await httpHandler.put({
+                    url: `${firebaseConfig.url}?print=silent`,
+                    body: JSON.stringify(collection)
+                })
+            ).then(x => console.log("success:" + x + " dt" + Object.keys(JSON.parse(JSON.stringify(collection)).data).length + " notadded:" + ImageCollection.notAddedCounter)).catch(err => console.error(err));
+    }).catch(err => console.error(err));
 
