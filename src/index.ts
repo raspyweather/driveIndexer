54be3a60c68ab69b32cc5d92a1353b826c7fe4d6
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-11-04 21:32:37 +0100

    updated json url

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import * as httpHandler from './httpHandler';
 import {ImageCollection} from "./imageCollection";
-import {drive} from "./drive";
-import {drives} from "./driveConfig";
 import {firebaseConfig} from "./firebaseConfig";
 import {FileDate} from "./FileDate";
+import {drives} from "./driveConfig";
+import {drive} from "./drive";
 
 
 console.log('drive indexer started');
-
 Promise.all(drives.map(config => new drive(config.key, config.folder).driveResult))
     .then(async results => {
         const collection = new ImageCollection();
         results.map(driveResult => collection.addImages(driveResult.files, driveResult.apiKey));
-        await uploadAll(collection);
-        //  await uploadToday(collection);
+        //  await uploadAll(collection);
+        await uploadToday(collection);
     }).then(() => console.log("data grabbed and processed ;)"))
     .catch(err => console.error(err));
 
 async function uploadToday(collection: ImageCollection) {
     const dateToday = FileDate.fromDate(new Date());
     const today = collection.getDateNode(dateToday);
+    //console.log(`${firebaseConfig.baseUrl}/data/${dateToday.year}/data/${dateToday.month}/data/${dateToday.day}.json?print=silent`)
     await httpHandler.put({
-        url: `${firebaseConfig.baseUrl}?print=silent`,
+        url: `${firebaseConfig.baseUrl}/data/${dateToday.year}/data/${dateToday.month}/data/${dateToday.day}.json?print=silent`,
         body: JSON.stringify(today)
     });
 }
